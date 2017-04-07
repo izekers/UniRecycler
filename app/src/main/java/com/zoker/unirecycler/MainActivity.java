36e25794de2e792de82b6d9aa0924d39c0579f3b
Zoker <1102412457@qq.com>
2017-04-07 17:42:28 +0800

    Initial commit

---
+++ app/src/main/java/com/zoker/unirecycler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zoker.unirecycler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //创建分支12312
+    }
+}
